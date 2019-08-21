de9952a606169fea77a93dba5ad1ce09ed7acf9b
niso120b <nissim.bitan@aquasec.com>
2019-08-21 11:58:34 +0300

    Update

--- microscanner/entrypoint.sh
+++ microscanner/entrypoint.sh
@@ -2,14 +2,4 @@
 
 echo "Starting..."
 
-sudo cat << EOT >> Dockerfile
-FROM ${image}
-
-ARG TOKEN
-ADD https://get.aquasec.com/microscanner /
-RUN chmod +x /microscanner
-RUN /microscanner ${token}
-RUN echo "No vulnerabilities!"
-EOT
-
-sudo sh -c "docker build $* --build-arg image="${IMAGE}" --no-cache ."
+sh -c "docker build $* --build-arg image="${IMAGE}" --no-cache ."
